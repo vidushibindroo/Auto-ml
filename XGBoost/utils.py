035d91b735bfa95ae04c199a02f1ef5cfa79fd1e
turtle-patronus <shriyaditaroy1999@gmail.com>
2020-07-07 00:41:06 +0530

    removed unnecessary fns

--- XGBoost/utils.py
+++ XGBoost/utils.py
@@ -13,14 +13,6 @@ class sigmoid_func():
     def gradient(self, x):
         return self.__call__(x) * (1 - self.__call__(x))
 
-
-
-def ema(running, new, gamma=.9):
-##ema is exponential moving average##
-    return gamma * running + (1. - gamma) * new
-
-
-
 def mean_squared_error(y_true, y_pred):
     mse = np.mean(np.power(y_true - y_pred, 2))
     return mse
@@ -36,3 +28,15 @@ def to_categorical(y):
     one_hot = np.zeros((m, col))
     one_hot[np.arange(m), y] = 1
     return one_hot
+
+def divide(X, feature_i, threshold):
+    split_func = None
+    if isinstance(threshold, int) or isinstance(threshold, float):
+        split_func = lambda sample: sample[feature_i] >= threshold
+    else:
+        split_func = lambda sample: sample[feature_i] == threshold
+
+    X_1 = np.array([sample for sample in X if split_func(sample)])
+    X_2 = np.array([sample for sample in X if not split_func(sample)])
+
+    return np.array([X_1, X_2])
