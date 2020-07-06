3def49d921f0f748c85e7fd4944fbdc8865c5b27
turtle-patronus <shriyaditaroy1999@gmail.com>
2020-07-07 00:48:28 +0530

    updated NB

---
+++ NaiveBayes/Naive Bayes.py
@@ -0,0 +1,42 @@
+import numpy as np
+from random import randrange
+import csv
+import math
+import sys
+import NaiveBayes.Utils_NB as utils
+
+eps = 1e-4
+class naive_bayes():
+    def fit(self, X, y):
+        self.X, self.y = X, y
+        self.classes = np.unique(y)
+        self.parameters = []
+        for i, c in enumerate(self.classes):
+            X_where_c = X[np.where(y == c)]
+            self.parameters.append([])
+            for col in X_where_c.T:
+                parameters = {"mean": col.mean(), "var": col.var()}
+                self.parameters[i].append(parameters)
+
+    def likelihood(self, mean, var, x):
+        coeff = 1.0 / math.sqrt(2.0 * math.pi * var + eps)
+        exponent = math.exp(-(math.pow(x - mean, 2) / (2 * var + eps)))
+        return coeff * exponent
+
+    def prior(self, c):
+        frequency = np.mean(self.y == c)
+        return frequency
+
+    def classification(self, sample):
+        posteriors = []
+        for i, c in enumerate(self.classes):
+            posterior = self._calculate_prior(c)
+            for feature_value, params in zip(sample, self.parameters[i]):
+                likelihood = self._calculate_likelihood(params["mean"], params["var"], feature_value)
+                posterior *= likelihood
+            posteriors.append(posterior)
+        return self.classes[np.argmax(posteriors)]
+
+    def predict(self, X):
+        y_pred = [self._classify(sample) for sample in X]
+        return y_pred
