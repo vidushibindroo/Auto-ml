dadc79fb7fa4fe81c6be6723b808aa3aaf66fd84
turtle-patronus <shriyaditaroy1999@gmail.com>
2020-07-11 12:50:38 +0530

    seed=2 in example NB

--- NaiveBayes/nb_example.py
+++ NaiveBayes/nb_example.py
@@ -13,7 +13,7 @@ def main():
     X = normalize(data.data)
     y = data.target
 
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.4)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.4, seed=2)
 
     clf = naive_bayes()
     clf.fit(X_train, y_train)
